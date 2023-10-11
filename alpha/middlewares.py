dd6023cc1d9fb9fbd166ee2df65447edb3fb5783
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-10-11 11:22:55 -0500

    comment out middleware for development

--- alpha/middlewares.py
+++ alpha/middlewares.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from alpha.models import AppKeys
+# from alpha.models import AppKeys
 
 
 class VerifyAPIKeyMiddleware:
@@ -12,23 +12,23 @@ class VerifyAPIKeyMiddleware:
         return self.get_response(request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.path.startswith("/api/auth/") or request.path.startswith("/admin/"):
-            return None
+        # if request.path.startswith("/api/auth/") or request.path.startswith("/admin/"):
+        #     return None
 
-        api_key = request.META.get("HTTP_API_KEY")
+        # api_key = request.META.get("HTTP_API_KEY")
 
-        if not api_key:
-            return JsonResponse({
-                "status": False,
-                "message": "'api_key' is missing from request headers."
-            }, status=400)
+        # if not api_key:
+        #     return JsonResponse({
+        #         "status": False,
+        #         "message": "'api_key' is missing from request headers."
+        #     }, status=400)
 
-        app_name, app_key = api_key.split(" ")
-        auth = AppKeys.check_key(app_key, app_name)
+        # app_name, app_key = api_key.split(" ")
+        # auth = AppKeys.check_key(app_key, app_name)
 
-        if not auth:
-            return JsonResponse({
-                "message": "Unauthorized"
-            }, status=401)
+        # if not auth:
+        #     return JsonResponse({
+        #         "message": "Unauthorized"
+        #     }, status=401)
 
         return None
