e9b89cdf97ec1848af67c5241d96cc0fef1fb16d
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-10-14 14:36:07 -0500

    added client admin form

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from .models import User, UserMeta
+from .forms import UserChangeForm, UserCreationForm, GroupAdminForm
 
-# Register your models here.
+
+class UserMetaAdmin(admin.StackedInline):
+    model = UserMeta
+    extra = 0
+
+
+class MyUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('username', 'first_name', 'last_name', 'email',
+                    'is_superuser', 'is_active')
+    list_filter = [
+        'is_superuser',
+        'is_staff',
+        'is_active',
+    ]
+    fieldsets = (
+        ("User Info", {'fields': ('first_name', 'last_name',
+                                  'username', 'email', 'password')}),
+        ('Permissions', {
+         'fields': ('is_superuser', 'is_staff', 'is_active')}),
+        ("Misc.", {"fields": ("date_joined", "last_login")})
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email', 'username', "first_name",
+                     "last_name")
+    readonly_fields = ("date_joined", "last_login")
+    ordering = ('email',)
+    filter_horizontal = ()
+    inlines = (UserMetaAdmin,)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+
+
+admin.site.unregister(Group)
+admin.site.register(User, MyUserAdmin)
+admin.site.register(Group, GroupAdmin)
---
+++ client/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+# from django.core.exceptions import ValidationError
+# from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.auth.models import Group
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    # username = forms.CharField(label='username', min_length=5, max_length=150)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+    # def username_clean(self):
+    #     username = self.cleaned_data['username'].lower()
+    #     new = User.objects.filter(username=username)
+    #     if new.count():
+    #         raise ValidationError("User Already Exist")
+    #     return username
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """
+    A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField(label=("Password"),
+                                         help_text=("Raw passwords are not stored, so there is no way to see "
+                                                    "this user's password, but you can change the password "
+                                                    "using <a href=\"../password/\">this form</a>."))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email',
+                  'password', 'is_active', 'is_superuser']
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class GroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        exclude = []
+
+    # Add the users field.
+    users = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        # Use the pretty 'filter_horizontal widget'.
+        widget=FilteredSelectMultiple('users', False)
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Do the normal form initialisation.
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+        # If it is an existing group (saved objects have a pk).
+        if self.instance.pk:
+            # Populate the users field with the current Group users.
+            self.fields['users'].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        # Add the users to the Group.
+        self.instance.user_set.set(self.cleaned_data['users'])
+
+    def save(self, *args, **kwargs):
+        # Default save
+        instance = super(GroupAdminForm, self).save()
+        # Save many-to-many data
+        self.save_m2m()
