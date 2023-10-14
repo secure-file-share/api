6630aa425a5ded4e474f1665ea06e097ad7f10d7
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-10-14 14:41:05 -0500

    updated admin page settings

--- alpha/admin.py
+++ alpha/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = "SecureFileShare Administration"
+admin.site.index_title = "SecureFileShare Admin"
+admin.site.site_title = "SecureFileShare"
