03bfaed70bc0aa6c4a6566796f606fc661fefdeb
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-10-14 15:34:04 -0500

    list out organization user inline

--- organization/admin.py
+++ organization/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from client.models import UserMeta
 from .models import Organization
+
+
+class OrganizationUserInline(admin.TabularInline):
+    model = UserMeta
+    extra = 0
+    verbose_name = "Organization User"
+
+    readonly_fields = ("user",)
+
+    def has_delete_permission(self, request, obj):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return False
 
 
 class OrganizationAdmin(admin.ModelAdmin):
@@ -21,6 +36,8 @@ class OrganizationAdmin(admin.ModelAdmin):
     )
 
     readonly_fields = ("secret_key",)
+
+    inlines = (OrganizationUserInline,)
 
 
 admin.site.register(Organization, OrganizationAdmin)
