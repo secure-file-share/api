2fda2eb5fea42c184ee4eca9a80eec4e8bbe5f11
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-10-14 14:50:09 -0500

    added organization admin page

--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Organization
 
-# Register your models here.
+
+class OrganizationAdmin(admin.ModelAdmin):
+    model = Organization
+
+    list_display = ("name", "phone", "address_state", "address_zip")
+    search_fields = ("name", "phone", "address_street1", "address_street2",
+                     "address_city", "address_state", "address_country", "address_zip")
+    list_filter = ("address_state", "address_country")
+
+    fieldsets = (
+        ("Company", {
+            "fields": ("name", "phone", "secret_key")
+        }),
+        ("Address", {
+            "fields": ("address_street1", "address_street2", "address_city",
+                       "address_state", "address_country", "address_zip")
+        }),
+    )
+
+    readonly_fields = ("secret_key",)
+
+
+admin.site.register(Organization, OrganizationAdmin)
