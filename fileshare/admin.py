fb9fb402966d6bd80e874944645351a5b233418c
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-10-14 14:58:09 -0500

    added file admin

--- fileshare/admin.py
+++ fileshare/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Files, FileShare
 
-# Register your models here.
+
+class FilesAdmin(admin.ModelAdmin):
+    model = Files
+
+    list_display = ("name", "ext", "size", "organization")
+    search_fields = ("name", "organization", "uploaded_by__username")
+    list_filter = ("ext", "expiration")
+
+    fieldsets = (
+        ("File", {
+            "fields": ("file_instance", "expiration", "organization")
+        }),
+        ("Details", {
+            "fields": ("name", "ext", "size")
+        })
+    )
+
+    readonly_fields = ("ext", "size")
+
+
+admin.site.register(Files, FilesAdmin)
