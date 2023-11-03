17d50082eeeab7db547d8cdd72694ebb0e008a64
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-11-03 17:03:36 -0500

    user apis url

--- client/urls.py
+++ client/urls.py
@@ -7,6 +7,8 @@ app_name = "client"
 # REST API ROUTER
 router = routers.SimpleRouter()
 
+router.register(r'user', viewsets.ClientUserViewSet, basename="user")
+router.register(r'search', viewsets.UserViewSet, basename="search")
 
 # URLS
 urlpatterns = []
