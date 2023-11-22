b6b885c4af98db0f59c4f5463e00ecc73e9737f8
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-11-22 17:02:33 -0600

    updated command to delete temporary files as well

--- alpha/management/commands/delete_expired_files.py
+++ alpha/management/commands/delete_expired_files.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pytz
 from datetime import datetime
@@ -8,20 +9,23 @@ from alpha.utilities import parse_date
 
 
 class Command(BaseCommand):
-    help = 'Delete expired files.'
+    help = 'Delete expired and temporary files.'
 
     def handle(self, *args, **options):
         now = datetime.now().astimezone(pytz.timezone(settings.TIME_ZONE))
 
         expired_files = Files.objects.filter(expiration__lte=now)
+        temp_files = settings.BASE_DIR / "temp"
 
         sys.stdout.flush()
         print("-" * 100)
-        print("Delete expired files.")
+        print("Delete expired and temporary files.")
         print("Files to be deleted before expiration date =",
               parse_date(now, format="%b. %m, %Y %H:%M:%S"))
         print("-" * 100)
         print(expired_files.count(), "expired files found.")
+        print("-" * 100)
+        print(len(os.listdir(temp_files)), "temporary files found")
         sys.stdout.flush()
 
         counter = 0
@@ -45,5 +49,29 @@ class Command(BaseCommand):
 
         print("-" * 100)
         print(counter, "expired files deleted.")
+        sys.stdout.flush()
+
+        counter = 0
+        (dirpath, dirnames, filenames) = next(os.walk(temp_files))
+
+        for file_ in filenames:
+            print("-" * 100)
+            print("Deleting file", file_)
+            print("-" * 100)
+            sys.stdout.flush()
+
+            try:
+                os.remove(os.path.join(dirpath, file_))
+            except Exception as e:
+                print("Unable to delete.")
+                print(str(e))
+            else:
+                print("Deleted")
+                counter += 1
+
+            sys.stdout.flush()
+
+        print("-" * 100)
+        print(counter, "temporary files deleted.")
         print("-" * 100)
         sys.stdout.flush()
