f1e3fb06b438071f0abccf2fc1de0f5bc0194b2f
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-12-05 10:18:14 -0600

    remove download file button from the admin page

--- fileshare/admin.py
+++ fileshare/admin.py
@@ -1,9 +1,9 @@
-from django.http import FileResponse
-from django.contrib import admin, messages
+# from django.http import FileResponse
+from django.contrib import admin  # , messages
 from django_object_actions import DjangoObjectActions
 from alpha.utilities import parse_date
 from .models import Files, FileShare
-from .crypto import decrypt_file
+# from .crypto import decrypt_file
 
 
 # class FileShareInline(admin.StackedInline):
@@ -39,22 +39,22 @@ class FilesAdmin(DjangoObjectActions, admin.ModelAdmin):
 
     readonly_fields = ("ext", "size")
 
-    change_actions = ("download_file_btn",)
+    # change_actions = ("download_file_btn",)
 
     # inlines = (FileShareInline,)
 
-    def download_file_btn(self, request, obj):
-        # GET KEY TO DECRYPT FILE
-        key = obj.organization.secret_key
+    # def download_file_btn(self, request, obj):
+    #     # GET KEY TO DECRYPT FILE
+    #     key = obj.organization.secret_key
 
-        # DECRYPT FILE
-        decrypted_file = decrypt_file(key, obj.file)
+    #     # DECRYPT FILE
+    #     decrypted_file = decrypt_file(key, obj.file)
 
-        return FileResponse(open(decrypted_file, "rb"))
+    #     return FileResponse(open(decrypted_file, "rb"))
 
-    download_file_btn.label = "Download Decrypted File"
-    download_file_btn.attrs = {
-        "class": "btn btn-block btn-primary mb-3"}
+    # download_file_btn.label = "Download Decrypted File"
+    # download_file_btn.attrs = {
+    #     "class": "btn btn-block btn-primary mb-3"}
 
     def file_size(self, obj):
         return "{:0.2f} MB".format(obj.size/1024)
@@ -77,28 +77,28 @@ class FileShareAdmin(DjangoObjectActions, admin.ModelAdmin):
     readonly_fields = ("unique_code", "name", "size",
                        "shared_by", "expiration", "organization")
 
-    change_actions = ("download_file_btn",)
+    # change_actions = ("download_file_btn",)
 
-    def download_file_btn(self, request, obj):
-        if not obj.pk:
-            messages.warning(request, "No file found.")
-            return
+    # def download_file_btn(self, request, obj):
+    #     if not obj.pk:
+    #         messages.warning(request, "No file found.")
+    #         return
 
-        try:
-            # GET KEY TO DECRYPT FILE
-            key = obj.organization.secret_key
+    #     try:
+    #         # GET KEY TO DECRYPT FILE
+    #         key = obj.organization.secret_key
 
-            # DECRYPT FILE
-            decrypted_file = decrypt_file(key, obj.file)
+    #         # DECRYPT FILE
+    #         decrypted_file = decrypt_file(key, obj.file)
 
-        except Exception as e:
-            messages.error(request, str(e))
-        else:
-            return FileResponse(open(decrypted_file, "rb"), as_attachment=True)
+    #     except Exception as e:
+    #         messages.error(request, str(e))
+    #     else:
+    #         return FileResponse(open(decrypted_file, "rb"), as_attachment=True)
 
-    download_file_btn.label = "Download File"
-    download_file_btn.attrs = {
-        "class": "btn btn-block btn-primary mb-3"}
+    # download_file_btn.label = "Download File"
+    # download_file_btn.attrs = {
+    #     "class": "btn btn-block btn-primary mb-3"}
 
     def file_name(self, obj):
         return f"{obj.file_instance.name}.{obj.file_instance.ext}"
