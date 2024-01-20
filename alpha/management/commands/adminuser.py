e79f8ffe4a04f5c25bdaf7ad8fdb4ff5890435ce
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-20 16:26:33 -0600

    updated adminuser command

--- alpha/management/commands/adminuser.py
+++ alpha/management/commands/adminuser.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from django.core.management.base import BaseCommand  # , CommandError
 from client.models import User
 
@@ -7,12 +8,16 @@ class Command(BaseCommand):
     help = 'Create admin user'
 
     def handle(self, *args, **options):
-        user = User.objects.create(username="admin")
+        try:
+            user, created = User.objects.get_or_create(username="admin")
 
-        user.active = True
-        user.is_staff = True
-        user.is_superuser = True
+            user.active = True
+            user.is_staff = True
+            user.is_superuser = True
 
-        user.set_password(os.environ.get(
-            "ADMIN_USER_PASSWORD", 'strongpassword'))
-        user.save()
+            user.set_password(os.environ.get(
+                "ADMIN_USER_PASSWORD", 'strongpassword'))
+            user.save()
+        except Exception as e:
+            print(e)
+            sys.stdout.flush()
